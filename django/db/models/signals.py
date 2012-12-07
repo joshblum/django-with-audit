9ce08e4286979a7eb11793f081d58388a53a9956
Joshua Blum <joshblum@mit.edu>
2012-12-07 15:49:04 -0500

    django mod, cannot find repo of 1.4.2 to fork :( '
    :

---
+++ django/db/models/signals.py
@@ -0,0 +1,20 @@
+from django.dispatch import Signal
+
+class_prepared = Signal(providing_args=["class"])
+
+pre_init = Signal(providing_args=["instance", "args", "kwargs"])
+post_init = Signal(providing_args=["instance"])
+
+#django audit-get-signal
+pre_get = Signal(providing_args=["instance", "args", "kwargs"])
+post_get = Signal(providing_args=["instance"])
+
+pre_save = Signal(providing_args=["instance", "raw", "using"])
+post_save = Signal(providing_args=["instance", "raw", "created", "using"])
+
+pre_delete = Signal(providing_args=["instance", "using"])
+post_delete = Signal(providing_args=["instance", "using"])
+
+post_syncdb = Signal(providing_args=["class", "app", "created_models", "verbosity", "interactive"])
+
+m2m_changed = Signal(providing_args=["action", "instance", "reverse", "model", "pk_set", "using"])
